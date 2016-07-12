0951c716ea1c02995a964aeb5d367a869a994939
Eadred <eadred.birchenough@zuhlke.com>
2016-07-12 09:30:06 +0100

    Initial template code

---
+++ src/Template.elm
@@ -0,0 +1,16 @@
+module Template exposing (..)
+
+{-|
+
+# Put documentation here
+
+@docs foo
+-}
+
+-- Imports go here
+
+{-|
+Documentaation for the method
+-}
+foo : Int
+foo = 0
---
+++ test/TestMain.elm
@@ -0,0 +1,19 @@
+module TestMain exposing (main)
+
+import ElmTest exposing (..)
+import Template exposing (..)
+
+fooTest : Test
+fooTest =
+  test
+  "First test"
+  (assertEqual 0 foo)
+
+tests : Test
+tests =
+  suite "All tests"
+  [ fooTest
+  ]
+
+main : Program Never
+main = runSuite tests
